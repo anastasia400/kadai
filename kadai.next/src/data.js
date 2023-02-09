25dfadb900bec1258aa2b98c39d1179df70bd912
anastasia400 <k.yasumi.sys21@morijyobi.ac.jp>
2023-02-09 12:35:20 +0900

    dadarian

--- kadai.next/src/data.js
+++
@@ -1,7 +0,0 @@
-export default [
-    { name: 'taro', mail: 'taro@yamada', age: 39 },
-    { name: 'hanko', mail: 'hanko@flower', age: 28 },
-    { name: 'sachiko', mail: 'sachiko@happy', age: 17 },
-    { name: 'jiro', mail: 'jiro@change', age: 6 },
-    { name: 'nami', mail: 'nami@mumemo', age: 50 },
-  ];
